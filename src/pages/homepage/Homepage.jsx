7b15c23a8cf051b8330bafcc3d0b101624c6cd7b
ML Pivniceru <mcpivniceru@gmail.com>
2020-01-29 22:28:34 +0000

    wip create folder structure

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Homepage.scss";
+
+const Homepage = () =>{
+    return(
+     <div className="homepage">
+         <div className="directory-menu">
+             
+         </div>
+     </div>
+    )
+}
+
+export default Homepage;
