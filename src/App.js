e7ac78db8ef45066ca6cc8be8f9d58264a142c43
ML Pivniceru <mcpivniceru@gmail.com>
2020-01-30 12:14:40 +0000

    added preview-collection component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Homepage from "./pages/homepage/Homepage"
 import {Route} from "react-router-dom";
+import ShopPage from "./pages/shop/shop"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Route exact path="/" component={Homepage}/>
+      <Route exact path="/shop" component={ShopPage}/>
     </div>
   );
 }
