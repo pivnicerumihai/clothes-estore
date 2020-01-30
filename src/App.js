a2c258ebb7962109426a9417b3c29dc41c125154
ML Pivniceru <mcpivniceru@gmail.com>
2020-01-30 22:07:34 +0000

    added firebase integration and google sign in + signin/signup form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import Homepage from "./pages/homepage/Homepage"
-import {Route} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop"
+import Header from "./components/header/header"
+import SignInSignUp from "./pages/signInSignUp/signInSignUp"
+import { auth } from "./firebase/firebase.utils"
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user})
+      console.log(user);
+    })
+  
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
   return (
     <div className="App">
+      <Header currentUser={this.state.currentUser} />
+      <Switch>
       <Route exact path="/" component={Homepage}/>
       <Route exact path="/shop" component={ShopPage}/>
+      <Route exact path="/signin" component={SignInSignUp}/>
+      </Switch>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/pages/signInSignUp/signInSignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./signInSignUp.scss";
+import SignIn from "../../components/signIn/signIn"
+
+const SignInSignUp = () =>{
+    return(
+        <div className="signInSignUp">
+            <SignIn/>
+        </div>
+    )
+}
+
+export default SignInSignUp;
